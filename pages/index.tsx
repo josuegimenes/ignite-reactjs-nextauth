e151b7c2895844cded348a1f22c469870b5d7950
Josué Gimenes <josuegalvares@gmail.com>
2021-09-12 19:49:34 -0300

    Validating Visitor

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps } from "next";
 import { FormEvent, useContext, useState } from "react"
 import { parseCookies } from 'nookies'
 import { AuthContext } from "../contexts/AuthContext";
+import { withSSRGuest } from "../utils/withSSRGuest";
 
 export default function Home() {
   const [email, setEmail] = useState('')
@@ -29,19 +30,8 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx);
-  
-  if (cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false,
-      }
-    }
-  }
-
+export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
     props: {}
   }
-}
+})
